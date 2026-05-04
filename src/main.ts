0dc9580a25b1e3cd9019ac781e62734d97d688e2
TianYouH <1406957473@qq.com>
2026-05-04 09:26:17 +0800

    docs(main.ts): 添加引导函数注释说明全局配置限制
    解释为什么在引导函数中只能初始化全局配置而不能使用依赖注入服务

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,16 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { GlobalExceptionFilter } from './filters/http-exception.filter';
 
+// 应用引导函数：负责创建 Nest 应用实例并启动, 并初始化一些全局配置,按照顺序执行
+// 注意：在这里初始化模块没办法使用到依赖注入其它服务，因为这是应用启动时调用的，而依赖注入是在模块加载时调用的
+// 所以，在这里只能初始化一些全局配置，比如日志中间件、全局异常过滤器等，而不能初始化模块，因为模块是在应用启动时加载的，而应用启动时依赖注入是无法使用的
+// 当然，如果有这类需要初始化，可以在 AppModule 中进行配置
+
 async function bootstrap() {
   // 创建 Nest 应用实例
   const app = await NestFactory.create(AppModule);
 
-  // 使用全局验证管道：自动验证 DTO
+  // 使用全局验证管道：自动验证 DTO，直接使用new实例化 的普通类，不是由 NestJS 依赖注入系统管理
   app.useGlobalPipes(new ValidationPipe());
 
   // 使用全局异常过滤器：统一处理所有异常
